8679af0426c915a8391a08c61959cf1b49e6dd5c
Gabriel Goncalves Albanez <00001089124053sp@aluno.educacao.sp.gov.br>
2023-09-13 10:54:31 -0300

    config ip

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default async function Home() {
 
-  const request = await fetch("http://localhost:3000/api/champions", {
+  const request = await fetch("http://10.112.240.187:3000/api/champions", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
