b0ca62e65b0167dd50a6836bcf5de045d647893b
GabrielAlbanez <gabriel.g.albabez@gmail.com>
2023-09-18 22:12:21 -0300

    7 teste deploy

--- app/champions/[name]/page.jsx
+++ app/champions/[name]/page.jsx
@@ -1,12 +1,12 @@
-
 // import { url } from "inspector";
 import Image from "next/image";
-import Logo from "@/assets/imgs/icons.webp"
-import CarrouselSkin from "@/app/components/CarrouselSkin"
+import Logo from "@/assets/imgs/icons.webp";
+import CarrouselSkin from "@/app/components/CarrouselSkin";
+export const runtime = "edge";
 
 async function getChampions(championsName) {
   const request = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/champions?${championsName}`,
+    `${process.env.NEXT_PUBLIC_APP_URL}/api/champions?${championsName}`,
     {
       method: "GET",
       headers: {
@@ -26,8 +26,7 @@ export default async function Champions({ params }) {
   const backgroundImageStyle = {
     backgroundImage: `url(${championFiltrado.img})`,
     backdropFilter: "blur(100%)", // Aqui adicionamos o desfoque com backdrop-filter
-    backgroundPosition : 'right 55% bottom 75%',
-    
+    backgroundPosition: "right 55% bottom 75%",
   };
 
   return (
@@ -36,7 +35,6 @@ export default async function Champions({ params }) {
         className={`bg-cover object-cover opacity-20  blur-md h-screen w-screen   absolute`}
         style={backgroundImageStyle}
       ></div>
-   
 
       {/* <Image src={championFiltrado.img} alt={championFiltrado.name} height={2000} width={1000}  className="w-full h-2/3 object-cover aspect-square bg-center"/> */}
       <div className="z-auto relative flex  justify-center items-center h-screen flex-col py-10">
@@ -44,22 +42,20 @@ export default async function Champions({ params }) {
           className={`  bg-cover   object-cover w-[67%] h-[50%]  sm:w-2/3 sm:h-4/5 aspect-square rounded-md flex items-end justify-center shadow-xl`}
           style={backgroundImageStyle}
         >
-          <h2 className="text-white text-xl sm:text2xl md:text-3xl lg:text-4xl ">{championFiltrado.name}</h2>
+          <h2 className="text-white text-xl sm:text2xl md:text-3xl lg:text-4xl ">
+            {championFiltrado.name}
+          </h2>
         </div>
         <div className="bg-zinc-950 w-2/3 h-[46%] sm:h-3/5 opacity-25 text-white flex flex-col  items-center sm:flex-row  ">
-          <div
-            className="w-3/6 flex justify-center flex-col items-center mt-12 sm:my-24 "
-          >
-            
-           <Image src={Logo} alt="icon" width={50} height={50}/>
+          <div className="w-3/6 flex justify-center flex-col items-center mt-12 sm:my-24 ">
+            <Image src={Logo} alt="icon" width={50} height={50} />
             <h1>{championFiltrado.type}</h1>
           </div>
 
-        
-
           <div className="flex flex-wrap justify-center items-center h-[500px] sm:h-[100%] md:h-[100%] sm:w-[45%] md:w-[51%]  px-4  sm:px-4 xl:px-24          sm:border-l-[2px] sm:border-white ">
-            
-            <h1 className="text-justify text-md lg:text-xl xl:text-2xl     ">{championFiltrado.title}</h1>
+            <h1 className="text-justify text-md lg:text-xl xl:text-2xl     ">
+              {championFiltrado.title}
+            </h1>
           </div>
         </div>
 
@@ -71,7 +67,7 @@ export default async function Champions({ params }) {
         <div></div>
       </div>
       <>
-       <CarrouselSkin Champion={championFiltrado}/>
+        <CarrouselSkin Champion={championFiltrado} />
       </>
     </div>
   );
